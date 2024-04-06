af45a077d2945c0b3e50d5552eefb811876b3ff3
DamienLeMalETPA <damienlemaldlm@gmail.com>
2024-04-06 13:33:38 +0200

    Modal Window

--- config/config.js
+++ config/config.js
@@ -2,11 +2,13 @@ const urlParams = new URLSearchParams(window.location.search);
 const query = urlParams.get("search");
 
 let categories = [];
+let selectedGame = null;
+let selectedGameThumbnail = null;
+let selectedCategory = null;
 
 async function performSearch() {
 
     let a = query.replaceAll(" ","%20");
-    console.log(a);
 
     document.querySelector("#body p1").innerHTML = "Search result for : " + query;
     
@@ -14,51 +16,57 @@ async function performSearch() {
         "https://www.speedrun.com/api/v1/games?page=2&name=" + a
     )
         .then((response) => {
-        
-            let result = response.json()
-            result.then(data => {
-                data.data.forEach((game) => {
-                    let div = document.createElement("div");
-                    let img = new Image();
-                    img.src = game.assets["cover-large"].uri;
-                    div.className = "thumbnail";
-                    let name = document.createElement("span");
-                    name.innerHTML = game.names.international;
-                    div.appendChild(name);
-                    div.appendChild(img);
-                    document.getElementById('body').appendChild(div);
-                    div.onmouseleave = function() { gameleave(); }
-                    img.onmouseenter = function() { gameHover(game, div); }
-                    fetch(
-                        "https://www.speedrun.com/api/v1/games/" + game.id + "/categories"
-                    ).then(gameCate => {
-                        gameCate.json().then(data => {
-                            console.log(data);
-                            categories[game.id] = data;
-                        });
-                    });
-                    
-                })
-            })
+            response.json()
+                .then(data => {
+                    data.data.forEach((game) => {
+                        createVignette(game);
+                    })})
+                .catch(e => console.log(e))
         })
-        .catch((e) => {
-            console.log(e);
-        });
+        .catch(e => console.log(e));
 }
 
 performSearch();
 
 
+function createVignette (game) {
+    let div = document.createElement("div");
+    let img = new Image();
+    img.src = game.assets["cover-large"].uri;
+    div.className = "thumbnail";
+    let name = document.createElement("span");
+    name.innerHTML = game.names.international;
+    div.appendChild(name);
+    div.appendChild(img);
+    document.getElementById('body').appendChild(div);
+
+    div.onmouseleave = function() { gameleave(); }
+    img.onmouseenter = function() { gameHover(game, div); }
+
+    fetch(
+        "https://www.speedrun.com/api/v1/games/" + game.id + "/categories"
+    ).then(gameCate => {
+        gameCate.json().then(data => {
+            categories[game.id] = data;
+            categories[game.id]["name"] = game.names.international;
+        });
+    });
+}
+
 function gameHover (game, div) {
     let menu = document.getElementById("contextMenu");
     menu.innerHTML = "";
     menu.classList.remove("hidden");
-    categories[game.id].data.forEach(cate => {
+
+    categories[game.id].data.forEach((cate,index) => {
         if (cate.type != "per-game") return;
         let li = document.createElement("li")
         let a = document.createElement("a");
         a.innerHTML = cate.name;
-        a.setAttribute("href","../leaderboard.html?game="+game.id+"&category="+cate.id+"&count=20");
+        a.setAttribute("href","#");
+        a.setAttribute("onclick","selectedGameThumbnail=\""+ game.assets["cover-large"].uri + "\";selectedGame=\""+game.id+"\";selectedCategory=\""+index+"\";showModal();");
+        //selectedGame="+game.id+";selectedCategory="+cate.id+";"
+        //a.setAttribute("href","../leaderboard.html?game="+game.id+"&category="+cate.id+"&count=20");
         li.appendChild(a);
         menu.appendChild(li);
         div.appendChild(menu);
@@ -70,3 +78,33 @@ function gameleave () {
     menu.innerHTML = "";
     menu.classList.add("hidden");
 }
+
+function showModal () {
+    document.getElementById('modalWindow').classList.remove('hidden');
+    document.querySelector('#header h1').innerHTML = categories[selectedGame].name;
+    document.querySelector('#header h3').innerHTML = categories[selectedGame].data[selectedCategory].name;
+    document.querySelector('#header img').src = selectedGameThumbnail;
+    document.getElementById("game").value = selectedGame;
+    document.getElementById("category").value = categories[selectedGame].data[selectedCategory].id;
+}
+
+function hideModal () {
+    document.getElementById('modalWindow').classList.add('hidden');
+}
+
+function submitLeaderboard () {
+    window.location.href = "../leaderboard.html?game="+selectedGame+"&category="+categories[selectedGame].data[selectedCategory].id+"&count=20";
+    return false;
+}
+
+//Modal window
+const checkbox = document.getElementById('modalCheckbox');
+const animParam = document.getElementById('animParam');
+
+checkbox.addEventListener('change', (event) => {
+  if (event.currentTarget.checked) {
+    animParam.classList.remove("hidden");
+  } else {
+    animParam.classList.add("hidden");
+  }
+})
--- leaderboard.js
+++ leaderboard.js
@@ -1,16 +1,22 @@
 const urlParams = new URLSearchParams(window.location.search);
-const totalCount = urlParams.get('count') == 0 ? 999999 : urlParams.get('count')-1;
-const game = urlParams.get('game');
-const category = urlParams.get('category');
-const animated = true;// urlParams.get('animated');
-const stayCount = 3;//urlParams.get('stayCount');
-const switchCount = 7;//urlParams.get('switchCount');
-const time = 3000;//urlParams.get('time');
+const totalCount =
+    urlParams.get("count") == 0 ? 999999 : urlParams.get("count") - 1;
+const game = urlParams.get("game");
+const category = urlParams.get("category");
+let animated = urlParams.get("animated");
+if (animated == null) animated = false;
+else animated = true;
+const stayCount = parseInt(urlParams.get("stayCount"));
+const switchCount = parseInt(urlParams.get("switchCount"));
+const time = 6000 / urlParams.get("speed");
 
 async function getLeaderboard() {
     //"https://www.speedrun.com/api/v1/leaderboards/nd28z0ed/category/w20e4yvd"
     return fetch(
-        "https://www.speedrun.com/api/v1/leaderboards/" + game + "/category/" + category
+        "https://www.speedrun.com/api/v1/leaderboards/" +
+            game +
+            "/category/" +
+            category
     )
         .then((response) => response.json())
         .catch((e) => {
@@ -18,24 +24,29 @@ async function getLeaderboard() {
         });
 }
 
-async function getNextPlayer (runs, count, playerData) {
-    
+//Recursive function that fetch and display every player in the leaderboard one after the other because the order is important
+async function getNextPlayer(runs, count, playerData) {
     if (count > totalCount || runs.length <= count) {
         if (animated == true) startAnimation();
         return playerData;
     }
-    await playerData.push(fetch(runs[count].run.players[0].uri)
-        .then((player) => {
-            player.json().then((p) => {
-                setupName(p, count+1);
-                getNextPlayer(runs, count+1, playerData);
-            }).catch((error) => {
-                console.log(error); 
-            });
-        })
-        .catch((error) => {
-            console.log(error);
-        }));
+    await playerData.push(
+        fetch(runs[count].run.players[0].uri)//Get Player data
+            .then((player) => {
+                player
+                    .json()//Convert data to json
+                    .then((p) => {
+                        setupName(p, count + 1);
+                        getNextPlayer(runs, count + 1, playerData);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    );
 }
 
 function setupName(p, position) {
@@ -44,34 +55,30 @@ function setupName(p, position) {
 
     //create element
     const li = document.createElement("li");
-    li.classList.add("names2");
-    //li.innerHTML = `<span class="names2">${pName}</span>`;
+    li.classList.add("names");
     li.innerHTML = `${position + ". " + pName}`;
 
-    //style
-    let color1 =
-        p.data["name-style"].style == "gradient"
-            ? p.data["name-style"]["color-from"]
-            : p.data["name-style"].color;
-    let color2 =
-        p.data["name-style"].style == "gradient"
-            ? p.data["name-style"]["color-to"]
-            : p.data["name-style"].color;
+    //Setup colored name according to the player's settings
+    const pstyle = p.data["name-style"];
+    let color1 = "#FFF";
+    let color2 = "#FFF";
+
+    if (pstyle != null) {
+        //style
+        color1 = pstyle.style == "gradient" ? pstyle["color-from"] : pstyle.color;
+        color2 = pstyle.style == "gradient" ? pstyle["color-to"] : pstyle.color;
+    }
+
     li.style.backgroundImage = `linear-gradient(to right, ${color1.dark},${color2.dark})`;
 
     //add to html
     document.querySelector("#leaderboard").appendChild(li);
 }
 
-async function main() {
-    let data = await getLeaderboard();
-    await getNextPlayer (data.data.runs, 0, []);
-}
-
 /// Animation
 let list = [];
 
-function startAnimation () {
+function startAnimation() {
     list = document.querySelector("#leaderboard").children;
 
     //Hide everything
@@ -83,19 +90,23 @@ function startAnimation () {
     resetAnimation();
 }
 
-function resetAnimation () {
+//When the recursive function gets to the end of the list, resets the cursors and start again
+function resetAnimation() {
     let cursorFrom = stayCount;
     let cursorTo = stayCount + switchCount;
 
-    setTimeout(function() { showNext(cursorFrom,cursorTo) },1000);
+    setTimeout(function () {
+        showNext(cursorFrom, cursorTo);
+    }, 1000);
 }
 
-function showNext (cursorFrom, cursorTo) {
+//recursive function that show and hide the next set of names
+function showNext(cursorFrom, cursorTo) {
     for (var i = cursorFrom; i < cursorTo; i++) {
         if (i >= list.length) continue;
-        show(list[i])
+        show(list[i]);
     }
-    setTimeout(function() {
+    setTimeout(function () {
         for (var i = cursorFrom; i < cursorTo; i++) {
             if (i >= list.length) continue;
             hide(list[i]);
@@ -103,32 +114,35 @@ function showNext (cursorFrom, cursorTo) {
         if (cursorTo >= list.length) {
             resetAnimation();
             return;
-        }else{
-            setTimeout(function() { showNext(cursorFrom+switchCount,cursorTo+switchCount) },1000);
-        } 
-    },time);
-
-    
-    
+        } else {
+            setTimeout(function () {
+                showNext(cursorFrom + switchCount, cursorTo + switchCount);
+            }, 1000);
+        }
+    }, time);
 }
 
-
-
-function hide (element) {
-    element.classList.add("fade-out");
-    setTimeout(function() {
-        element.classList.remove("fade-out");
-        element.classList.add("hide");  
-    }, 1000);
-    
-}
-
-function show (element) {
+//Shows the name with an animation
+function show(element) {
     element.classList.remove("hide");
     element.classList.add("fade-in");
-    setTimeout(function() {
+    setTimeout(function () {
         element.classList.remove("fade-in");
-    },1000);
+    }, 1000);
+}
+
+//Hides the name with an animation
+function hide(element) {
+    element.classList.add("fade-out");
+    setTimeout(function () {
+        element.classList.remove("fade-out");
+        element.classList.add("hide");
+    }, 1000);
+}
+
+async function main() {
+    let data = await getLeaderboard();
+    await getNextPlayer(data.data.runs, 0, []);
 }
 
 main();
